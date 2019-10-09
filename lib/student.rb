1aa9517a202b4efacd968f34e3b7ca83c1b2e141
Srikalyan Megati <kalmegati@gmail.com>
2019-10-09 14:09:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,13 @@
 class Student < User
 
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+  end
   
+  def knowledge
+    @knowledge
+  end
 
 end
